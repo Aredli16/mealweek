f7c4fee6b61048a5cf5677905dc0dacd02e55985
aredli <xtarak@gmail.com>
2021-10-12 20:56:37 +0200

    Import licence for google font

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mealweek/screens/homescreen.dart';
 
-void main() => runApp(const MealWeek());
+void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+  runApp(const MealWeek());
+}
 
 class MealWeek extends StatelessWidget {
   const MealWeek({Key? key}) : super(key: key);
